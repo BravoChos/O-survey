db8f2dc7e3726afb0d3fc0bf2596686d25cd132d
Seonghyun Cho <deeplearner507@gmail.com>
2018-10-28 19:40:42 +0900

    install create-react-app

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ Technical Stack: JavaScript, node.js, express, REACT, REDUX, MongoDB
 - Automatically send massive amount of surveys to customer and request feedback.
 - Tabulate the feedback and provide suggestion to user along with the result.
 - Handle credit cards and receive payments with Stripe
+
+Google+ API
+-> using credentials with thie API
+Accessing user data with Oauth 2.0
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -9,14 +9,17 @@ module.exports = (app) => {
   app.get('/auth/google/callback', passport.authenticate('google'),
   (err, req, res, next) => { // custom error handler to catch any errors, such as TokenError
     if (err.name === 'TokenError') {
-     res.redirect('/auth/google'); // redirect them back to the login page
+      console.log("ok token error");
+      // res.redirect('/api/current_user');
+    //  res.redirect('/auth/google'); // redirect them back to the login page
     } else {
      // Handle other errors here
     }
   },
   (req, res) => { // On success, redirect back to '/'
     console.log("WHAT IS GOING ON");
-    // res.redirect('/surveys');
+    // res.redirect('/api/current_user');
+    res.send(req.user);
   })
 
   app.get('/api/logout', (req, res) => {
