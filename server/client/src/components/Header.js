ffc3d4abe67c00d60cf913e7332f95ca7745bbc4
Song Cho <deeplearner507@gmail.com>
2018-11-06 01:13:33 +0900

    create Payment component
    applied Stripe billing process  and created middleware for login check
    
    forgot to include middleware file...so...
    
    Handling Payments Done

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-
+import Payments from './Payments';
 
 class Header extends Component {
 
   renderContent(){
     switch (this.props.auth) {
       case null:
-       return 'Still deciding';
+        return 'Still deciding';
       case false:
         return <li><a href="/auth/google">Logined with Google</a></li>;
       default: 
-        return <li><a href="/api/logout">Logout</a></li>;
+        return [
+          <li key="1"><Payments /></li>,
+          <li key="3" style={{ margin: '0 10px' }}>
+            Credits: {this.props.auth.credits}
+          </li>,
+          <li key="2"><a href="/api/logout">Logout</a></li>
+        ];
     }
   }
 
