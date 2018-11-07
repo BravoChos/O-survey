263b87dac02cc3c1025e263abfb79208fa19b17e
Song Cho <deeplearner507@gmail.com>
2018-11-08 00:11:08 +0900

    Handling Payments Done

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -8,12 +8,15 @@ class Header extends Component {
   renderContent(){
     switch (this.props.auth) {
       case null:
-       return 'Still deciding';
+        return 'Still deciding';
       case false:
         return <li><a href="/auth/google">Logined with Google</a></li>;
       default: 
         return [
           <li key="1"><Payments /></li>,
+          <li key="3" style={{ margin: '0 10px' }}>
+            Credits: {this.props.auth.credits}
+          </li>,
           <li key="2"><a href="/api/logout">Logout</a></li>
         ];
     }
