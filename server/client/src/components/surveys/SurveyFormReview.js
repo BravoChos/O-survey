46ac24684efde76264cddad1ff9eb7ee0a28322e
Song Cho <deeplearner507@gmail.com>
2018-11-18 00:33:49 +0900

    posting the Surveys and if successful Redirect user back to /surveys

--- server/client/src/components/surveys/SurveyFormReview.js
+++ server/client/src/components/surveys/SurveyFormReview.js
@@ -1,27 +1,42 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import formFields from './formFields';
+import { withRouter } from 'react-router-dom';
+import _ from 'lodash';
+import * as actions from '../../actions';
 
 // SurveyFormReview shows users their form inputs for review
-const SurveyFormReview = ( { onCancel, formValues }) => {
+const SurveyFormReview = ( { onCancel, formValues, submitSurvey, history }) => {
+  // const reviewFields = _.map(formFields, field => {
+  const reviewFields = _.map(formFields, ({ name, label }) => {
+    return (
+      // <div key={field.name}>
+      <div key={name}>
+        <label>
+          {label}
+        </label>
+        <div>
+          {formValues[name]}
+        </div>
+      </div>
+    )
+  })
 
   return (
     <div>
       <h5>
         Please confirm your entries
       </h5>
-      <div>
-        <div>
-          <label>Survey Title</label>
-          <div>
-            {formValues.title}
-          </div>
-        </div>
-
-      </div>
-
-
-      <button className="yellow darken-3 btn-flat" onClick={onCancel} >
+      {reviewFields}
+      {/* <button className="yellow darken-3 btn-flat" onClick={onCancel} > */}
+      <button className="yellow darken-3 white-text btn-flat" onClick={onCancel} >
         Back
+      </button>
+      <button 
+        onClick={() => submitSurvey(formValues, history)}
+        className="green darken-3 btn-flat right white-text"
+      >
+        Send Survey <i className="material-icons right">email</i>
       </button>
     </div>
   );
@@ -35,4 +50,4 @@ function mapStateToProps(state){
 }
 
 
-export default connect(mapStateToProps)(SurveyFormReview);
+export default connect(mapStateToProps, actions)(withRouter(SurveyFormReview));
