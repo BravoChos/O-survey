33ca7555545df6a05bbfbd61db0c6c4acf889aaa
Seonghyun Cho <deeplearner507@gmail.com>
2018-11-18 07:39:14 -0500

    Processing Pipeline and Parsing the Route

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -3,6 +3,10 @@ const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 const Mailer = require('../services/Mailer');
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
+const _ = require('lodash');
+const Path = require('path-parser').default;
+// const Path = require('Typescript');
+const { URL } = require('url');
 
 const Survey = mongoose.model('surveys')
 
@@ -45,7 +49,24 @@ module.exports = app => {
   });
 
   app.post('/api/surveys/webhooks', (req, res)=>{
-    console.log(req.body);
-    res.send({});
+    //console.log(req.body);
+    // const events = _.map(req.body, (event) => {
+      const events = _.map(req.body, ({ email, url }) => {
+      // const pathname = new URL(event.url).pathname;
+      const pathname = new URL(url).pathname;   
+      const p = new Path('/api/surveys/:surveyId/:choice');
+      // console.log(p.test(pathname));
+      const match = p.test(pathname);
+      if (match) {
+        // return { email: email, surveyId: match.surveyId, choice: match.choice};
+        return { email, surveyId: match.surveyId, choice: match.choice };
+      }
+    });
+    //remove undefined
+    const compactEvents = _.compact(events);
+    // no duplicate
+    const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId' );
+    console.log(uniqueEvents);
+    // res.send({});
   })
 };
